de6d8b2d662609aa62f35268c5ada8be94ffa4a5
dszwarc <dszwarc93@gmail.com>
2023-04-14 15:58:59 -0400

    add CBV functions in view file. added url paths for event models

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),#homepage
     path('about/', views.about, name='about'),
-    path('events/', views.events_index, name='events_index'),
+    path('events/', views.EventList.as_view(), name='events_index'),
+    path('events/create', views.EventCreate.as_view(), name='event_create'),
+    path('events/<int:pk>', views.EventDetail.as_view(), name='event_detail'),
+    path('events/<int:pk>/delete', views.EventDelete.as_view(), name='event_delete'),
 ]
